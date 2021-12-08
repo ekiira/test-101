b55182c0d4611d0d1d9d88e74e63f80fd2d06aa6
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-12-08 23:04:41 +0100

    test starter

---
+++ src/App.test.js
@@ -0,0 +1,26 @@
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
+test("two plus two", () => {
+  const value = 2.34 + 2.1;
+  expect(value).toBeCloseTo(4.4, 1);
+});
+
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
